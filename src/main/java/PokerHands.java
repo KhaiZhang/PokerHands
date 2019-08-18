33b2c7526317f75be27e927eff53e6f78b2a101c
Khai <KhaiZhang@163.com>
2019-08-18 18:08:04 +0800

    [feat:implement compare two card with higtest value card]

--- src/main/java/PokerHands.java
+++ src/main/java/PokerHands.java
@@ -1,2 +1,68 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class PokerHands {
+
+    public String compareTwoCards(String player1Card, String player2Card) {
+        List<Integer> cards1 = conversionCard(player1Card);
+        List<Integer> cards2 = conversionCard(player2Card);
+        return cards1.get(4)>cards2.get(4)?"player1 win" : "player2 win";
+    }
+
+    public List<Integer> conversionCard(String cards){
+        ArrayList<Integer> result = new ArrayList<>();
+        String[] cardList = cards.split(" ");
+        for(String card : cardList){
+            switch (card.charAt(1)){
+                case '2':
+                    result.add(2);
+                    break;
+                case '3':
+                    result.add(3);
+                    break;
+                case '4':
+                    result.add(4);
+                    break;
+                case '5':
+                    result.add(5);
+                    break;
+                case '6':
+                    result.add(6);
+                    break;
+                case '7':
+                    result.add(7);
+                    break;
+                case '8':
+                    result.add(8);
+                    break;
+                case '9':
+                    result.add(9);
+                    break;
+                case 'T':
+                    result.add(10);
+                    break;
+                case 'J':
+                    result.add(11);
+                    break;
+                case 'Q':
+                    result.add(12);
+                    break;
+                case 'K':
+                    result.add(13);
+                    break;
+                case 'A':
+                    result.add(14);
+                    break;
+            }
+        }
+        result.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer integer1, Integer integer2) {
+                return integer2 - integer1;
+            }
+        });
+        return result;
+    }
+
 }
--- src/test/java/PokerHandsTest.java
+++ src/test/java/PokerHandsTest.java
@@ -1,5 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PokerHandsTest {
 
+    @Test
+    public void should_return_player2_win_when_given_cards_H1_H2_H3_H4_ST_and_C1_C2_C3_C4_SA(){
+        PokerHands pokerHands = new PokerHands();
+        String player1Card = "H5 H2 H3 H4 ST";
+        String player2Card = "C5 C2 C3 C4 SA";
+        String result = pokerHands.compareTwoCards(player1Card,player2Card);
+        Assert.assertEquals("player2 win",result);
+    }
 }
